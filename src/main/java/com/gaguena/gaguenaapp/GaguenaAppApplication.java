f542a0ac23653ac83108ebe93f405edfddd0219c
Gabriel Aguena <gaguena@gmail.com>
2019-09-26 23:24:01 -0300

    First Commit

---
+++ src/main/java/com/gaguena/gaguenaapp/GaguenaAppApplication.java
@@ -0,0 +1,13 @@
+package com.gaguena.gaguenaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaguenaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaguenaAppApplication.class, args);
+	}
+
+}
